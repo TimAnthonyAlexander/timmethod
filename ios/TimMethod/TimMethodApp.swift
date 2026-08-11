e33a0bed5a2d5f976c6eaf940fcd3f9a473f1366
Tim Anthony Alexander <info@timanthonyalexander.de>
2026-08-11 23:34:32 +0200

    iOS

---
+++ ios/TimMethod/TimMethodApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct TimMethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Tim Method")
+    }
+}
